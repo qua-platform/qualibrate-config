0c3f77087ab80860973fcc30f027539ae18d7669
Maxim V4S <maxim.d@v4scale.com>
2025-07-25 16:53:37 +0300

    feat: cli switch project

--- qualibrate_config/cli/project/commands/switch.py
+++ qualibrate_config/cli/project/commands/switch.py
@@ -1,11 +1,29 @@
+from pathlib import Path
+
 import click
+
+from qualibrate_config.cli.vars import CONFIG_PATH_HELP
+from qualibrate_config.core.project.switch import switch_project
+from qualibrate_config.vars import DEFAULT_CONFIG_FILEPATH
 
 __all__ = ["switch_command"]
 
 
 @click.command(name="switch")
-def switch_command() -> None:
-    pass
+@click.argument("project", type=str)
+@click.option(
+    "--config-path",
+    type=click.Path(
+        exists=False,
+        path_type=Path,
+    ),
+    default=DEFAULT_CONFIG_FILEPATH,
+    show_default=True,
+    help=CONFIG_PATH_HELP,
+)
+def switch_command(project: str, config_path: Path) -> None:
+    if switch_project(config_path, project):
+        click.echo(f"Project switched to '{project}'.")
 
 
 if __name__ == "__main__":
---
+++ qualibrate_config/core/project/switch.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import click
+
+from qualibrate_config.core.content import get_config_file_content, simple_write
+from qualibrate_config.core.project.path import get_project_path
+from qualibrate_config.vars import QUALIBRATE_CONFIG_KEY
+
+
+def switch_project(config_path: Path, project: str) -> bool:
+    project_path = get_project_path(config_path.parent, project)
+    if not project_path.exists():
+        click.secho(
+            (
+                f"Can't switch project to '{project}'. "
+                f"There is no specified project in {project_path.parent}."
+            ),
+            fg="red",
+        )
+        return False
+    common_config, config_file = get_config_file_content(config_path)
+    common_config[QUALIBRATE_CONFIG_KEY]["project"] = project
+    simple_write(config_file, common_config)
+    return True
