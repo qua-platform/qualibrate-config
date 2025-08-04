af56958a60db1b5f2ba73812ebfa6ca61d7e5881
Maxim V4S <maxim.d@v4scale.com>
2025-08-04 10:06:09 +0300

    feat: project switch optional raise if invalid project

--- qualibrate_config/core/project/switch.py
+++ qualibrate_config/core/project/switch.py
@@ -7,16 +7,18 @@ from qualibrate_config.core.project.path import get_project_path
 from qualibrate_config.vars import QUALIBRATE_CONFIG_KEY
 
 
-def switch_project(config_path: Path, project: str) -> bool:
+def switch_project(
+    config_path: Path, project: str, *, raise_if_error: bool = False
+) -> bool:
     project_path = get_project_path(config_path.parent, project)
     if not project_path.exists():
-        click.secho(
-            (
-                f"Can't switch project to '{project}'. "
-                f"There is no specified project in {project_path.parent}."
-            ),
-            fg="red",
+        error_msg = (
+            f"Can't switch project to '{project}'. "
+            f"There is no specified project in {project_path.parent}."
         )
+        if raise_if_error:
+            raise ValueError(error_msg)
+        click.secho(error_msg, fg="red")
         return False
     common_config, config_file = get_config_file_content(config_path)
     common_config[QUALIBRATE_CONFIG_KEY]["project"] = project
