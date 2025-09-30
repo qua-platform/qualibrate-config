3971d8209c844e6e67ffe139b2c0220a65df53c9
Maxim V4S <maxim.d@v4scale.com>
2025-09-30 14:48:21 +0300

    refactor: deprecated base migrations class

--- qualibrate_config/cli/utils/__init__.py
+++ qualibrate_config/cli/utils/__init__.py
@@ -1,9 +1,10 @@
 import functools
 import warnings
 from collections.abc import Callable
-from typing import Any, TypeVar
+from typing import Any, TypeVar, cast
 
 F = TypeVar("F", bound=Callable[..., Any])
+C = TypeVar("C", bound=type[Any])
 
 
 def deprecated_alias(
@@ -21,5 +22,29 @@ def deprecated_alias(
             return func(*args, **kwargs)
 
         return wrapped  # type: ignore
+
+    return decorator
+
+
+def deprecated_class_alias(
+    *, name: str, deprecated_module: str, new_module: str
+) -> Callable[[C], C]:
+    """Decorator for classes."""
+
+    def decorator(cls: C) -> C:
+        class Wrapped(cls):  # type: ignore[misc, valid-type]
+            def __init__(self, *args: Any, **kwargs: Any) -> None:
+                warnings.warn(
+                    f"'{name}' from '{deprecated_module}.{name}' "
+                    f"is deprecated. Use '{new_module}.{name}' instead.",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+                super().__init__(*args, **kwargs)
+
+        Wrapped.__name__ = cls.__name__
+        Wrapped.__doc__ = cls.__doc__
+        Wrapped.__module__ = cls.__module__
+        return cast(C, Wrapped)
 
     return decorator
